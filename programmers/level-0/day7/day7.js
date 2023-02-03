3cc68cef5bdb3798863549e87a3029638ebb632a
trevor1107 <qnrudwns123@naver.com>
2023-02-03 13:36:37 +0900

    modify programmers level-0 day7

--- programmers/level-0/day7/day7.js
+++ programmers/level-0/day7/day7.js
@@ -62,7 +62,7 @@ function solution(n, k) {
  * https://school.programmers.co.kr/learn/courses/30/lessons/120831
  * 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록
  * solution 함수를 작성해주세요.
- * 예) 10	=> 30 // 4 => 6
+ * 예) 10 => 30 // 4 => 6
  */
 function solution(n) {
   let answer = 0;
