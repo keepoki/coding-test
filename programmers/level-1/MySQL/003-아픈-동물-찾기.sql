25d24cffd0836c6edb726b1934bd3805e17c4d7f
trevor1107 <qnrudwns123@naver.com>
2023-02-14 22:17:14 +0900

    modify programmers level-1 002~004 설명 위치 변경

--- programmers/level-1/MySQL/003-아픈-동물-찾기.sql
+++ programmers/level-1/MySQL/003-아픈-동물-찾기.sql
@@ -6,12 +6,12 @@
 -- ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 
 -- 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
 
--- 문자열은 작은따옴표(') 또는 큰따옴표("_로 감싸면 된다.
--- Oracle과 MySQL의 차이점 중 하나는 문자열의 대소문자 구분이다.
--- Oracle은 대소문자를 구분하지 않는다.
--- MySQL은 기본적으로 대소문자를 구분하고, 변경이 가능하다.
-
 SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS
 WHERE INTAKE_CONDITION = "Sick"
 ORDER BY ANIMAL_ID
+
+-- 문자열은 작은따옴표(') 또는 큰따옴표("_로 감싸면 된다.
+-- Oracle과 MySQL의 차이점 중 하나는 문자열의 대소문자 구분이다.
+-- Oracle은 대소문자를 구분하지 않는다.
+-- MySQL은 기본적으로 대소문자를 구분하고, 변경이 가능하다.
