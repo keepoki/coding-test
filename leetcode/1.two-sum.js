542c1afc4df7bdebefa7952402f3e0337f514360
trevor1107 <qnrudwns123@naver.com>
2023-12-17 23:59:34 +0900

    modify: 1번 문제 설명 추가

--- leetcode/1.two-sum.js
+++ leetcode/1.two-sum.js
@@ -11,6 +11,7 @@
  * @return {number[]}
  */
 const twoSum = function(nums, target) {
+  // 최적화를 위해 map 자료구조르 활용하여 캐싱 처리를 한다.
   const numMap = new Map();
   let num, diff, cache;
   for (let i = 0; i < nums.length; ++i) {
@@ -25,7 +26,14 @@ const twoSum = function(nums, target) {
 };
 // @lc code=end
 
-// ** first **
+/*
+  정수로 이루어진 배열 nums와 목표로 하는 정수 target이 주어지는데
+  nums에서 임의의 숫자 두 개에 대한 합계가 target과 같은 것을 찾아서
+  임의의 숫자 두 개에 대한 인덱스를 배열로 리턴 해야한다.
+  동일한 인덱스는 리턴하지 못한다.
+ */
+
+// 처음 풀었던 방법
 // function twoSum(nums, target) {
 //   for (let i = 0; i < nums.length; ++i) {
 //     for (let j = i + 1; j < nums.length; ++j) {
@@ -35,4 +43,3 @@ const twoSum = function(nums, target) {
 //     }
 //   }
 // }
-
